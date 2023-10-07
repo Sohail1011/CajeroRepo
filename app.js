2dd34136a7ea5894f906c7206ad6d7aa9883b978
Sohail Iran <aratavalga@gmail.com>
2023-10-07 11:00:09 -0500

    DNI

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let cuentas = [
-    { nombre: "Mali", saldo: 200, password: 'helloworld' },
-    { nombre: "Gera", saldo: 290, password: 'l33t' },
-    { nombre: "Maui", saldo: 67, password: '123' }
+    { nombre: "Mali", saldo: 200, DNI: 5678902, password: 'helloworld' },
+    { nombre: "Gera", saldo: 290, DNI: 7456781, password: 'l33t' },
+    { nombre: "Maui", saldo: 67, DNI: 2256789, password: '123' }
 ]
 
 if (cuentas.saldo > 700) {
