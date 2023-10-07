97d9405fecc4fc10d9d2e716ce2e701244a50c30
Sohail Iran <aratavalga@gmail.com>
2023-10-07 09:52:20 -0500

    repoCash

---
+++ app.js
@@ -0,0 +1,11 @@
+let cuentas = [
+    { nombre: "Mali", saldo: 200, password: 'helloworld' },
+    { nombre: "Gera", saldo: 290, password: 'l33t' },
+    { nombre: "Maui", saldo: 67, password: '123' }
+]
+
+if (cuentas.saldo > 700) {
+
+} else {
+
+}
